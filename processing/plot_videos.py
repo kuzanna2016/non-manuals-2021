a157268200b4c5efbb11c42e6884e0d33bd5434f
kuzanna2016 <31651155+kuzanna2016@users.noreply.github.com>
2023-08-18 14:46:25 +0200

    add multiple sentences and brows

--- processing/plot_videos.py
+++ processing/plot_videos.py
@@ -13,8 +13,8 @@ parser.add_argument("--openface_fp", default=os.path.join(SAVE_TO, 'open_face_wi
                     help="Path to a .csv file with OpenFace")
 parser.add_argument("--elan_fp", default=os.path.join(SAVE_TO, 'elan_preprocessed.tsv'), type=str,
                     help="Path to an Elan file with annotations")
-parser.add_argument("--sentence", default=None, type=str,
-                    help="Sentence to plot")
+parser.add_argument("--sentences", type=str, nargs='*',
+                    help="Sentences to plot")
 parser.add_argument("--targets", default=[
     'perp_dist39_42_3d_pred_MLP_best_model_diff',
     'perp_dist39_42_3d_pred_MLP_best_model_no_features_diff',
@@ -22,10 +22,10 @@ parser.add_argument("--targets", default=[
                     help="Which targets to plot")
 parser.add_argument("--n_samples", default=5, type=int,
                     help="If no video name, random n_samples will be plotted")
-parser.add_argument("--brow", default=BROWS.INNER.value, type=str,
+parser.add_argument("--brows", default=BROWS.INNER.value, type=str, nargs='+',
                     help="The eyebrow distance to plot [inner, outer]")
 parser.add_argument("--plot_head", default='', type=str,
-                    help="Whether to plot the head rotation, specify wich axis if plot, like 'x', or 'xy' or 'xyz'")
+                    help="Whether to plot the head rotation, specify which axis if plot, like 'x', or 'xy' or 'xyz'")
 parser.add_argument("--save_to", default=os.path.join(SAVE_TO, PLOTS_FP), type=str,
                     help="Save path for logs of cross-validation")
 
@@ -34,32 +34,35 @@ def plot_samples(openface_fp,
                  elan_fp,
                  targets,
                  save_to,
-                 sentence=None,
+                 sentences=None,
                  n_samples=5,
-                 brow=BROWS.INNER.value,
+                 brows=[BROWS.INNER.value],
                  plot_head=False):
     df = pd.read_csv(openface_fp, index_col=[0, 1])
     elan = pd.read_csv(elan_fp, sep='\t')
-    if sentence is None:
+    if sentences is None:
         samples = random.sample(df.index.levels[0].tolist(), n_samples)
     else:
-        samples = [sentence]
+        samples = sentences
 
     n_cols = len(targets) + len(plot_head) if plot_head else len(targets)
-    fig, axes = plt.subplots(len(samples), n_cols, figsize=(8 * n_cols, 5 * len(samples)))
+    n_rows = len(samples)
+    fig, axes = plt.subplots(n_rows, n_cols, figsize=(8 * n_cols, 5 * n_rows))
     if not isinstance(axes, np.ndarray):
         axes = np.asarray([axes])
     if axes.ndim == 1:
         axes = np.expand_dims(axes, axis=0)
     for n, sample in enumerate(samples):
         for j, target in enumerate(targets):
-            name = f'{brow}_{target}'
-            axes[n][j].plot(df.loc[sample, name])
+            for brow in brows:
+                name = f'{brow}_{target}'
+                axes[n][j].plot(df.loc[sample, name])
             axes[0][j].set_title(target)
         if plot_head:
-            for k, pose_ax in plot_head:
+            for k, pose_ax in enumerate(plot_head):
                 name = f'pose_R{pose_ax}'
                 axes[n][j + 1 + k].plot(df.loc[sample, name])
+                axes[0][j + 1 + k].set_ylim(-1,1)
                 if pose_ax == 'x':
                     axes[n][j + 1 + k].invert_yaxis()
                 axes[0][j + 1 + k].set_title(name)
@@ -85,10 +88,12 @@ def plot_samples(openface_fp,
         ax.set_xlabel('')
         ax.tick_params(reset=True)
 
-    if sentence is not None:
-        f_name = os.path.join(save_to, f'{sentence}_{brow}_compare_targets')
+    brows_str = '_'.join(brows)
+    if sentences:
+        sentences_str = '_'.join(sentences)
+        f_name = os.path.join(save_to, f'{sentences_str}_{brows_str}_compare_targets')
     else:
-        f_name = os.path.join(save_to, f'{n_samples}_{brow}_samples_compare_changes')
+        f_name = os.path.join(save_to, f'{n_samples}_{brows_str}_samples_compare_changes')
     i = 1
     while os.path.isfile(f_name + '_' + str(i) + '.png'):
         i += 1
@@ -97,5 +102,5 @@ def plot_samples(openface_fp,
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
-    plot_samples(args.openface_fp, args.elan_fp, targets=args.targets, sentence=args.sentence, n_samples=args.n_samples, brow=args.brow,
+    plot_samples(args.openface_fp, args.elan_fp, targets=args.targets, sentences=args.sentences, n_samples=args.n_samples, brows=args.brows,
                  plot_head=args.plot_head, save_to=args.save_to)
